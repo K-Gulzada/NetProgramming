6e8e12d26d882fe8910c309c0abab8fd090e64fa
Кахар Гулзада <@lcoact.itstep>
2021-09-27 21:30:38 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example_04();
+        }
+
+    /*    public static void Example_01()
+        {
+            WebClient client = new WebClient();
+            Stream data = client.OpenRead("http://www.google.com"); // открой поток для чтения вот этого http://www.google.com
+
+            using (StreamReader reader = new StreamReader(data))
+            {
+                string str = reader.ReadToEnd();
+
+                Console.WriteLine(str);
+            }
+
+            data.Close();
+        }*/
+
+     /*   public static void Example_02(string url, string data)
+        {
+            byte[] postArr = Encoding.ASCII.GetBytes(data);
+
+            WebClient client = new WebClient();
+            Stream stream = client.OpenWrite(url);
+            stream.Write(postArr, 0, postArr.Length);
+            stream.Close();
+        }
+
+        public static void Example_03(string url, string data)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            string result = client.UploadString(url, data);
+            Console.WriteLine(result);
+        }*/
+
+        public static void Example_04()
+        {
+            WebRequest request = WebRequest.Create("http://www.google.com");
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+
+            Console.WriteLine(response.StatusDescription);
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            Console.WriteLine(reader.ReadToEnd());
+
+        }
+    }
+}
